a950b6506eece2e3cb912edebc4fb0e5502d414c
Chris Carne <cjcarne@gmail.com>
2022-02-16 00:25:31 +0000

    route protection completed

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {getGoals, setGoals, updateGoals, deleteGoals} = require('../controllers/goalControllers')
+const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalControllers')
+const { protect } = require('../middleware/authMiddleware')
 
+router.use(protect)
 router.route('/').get(getGoals).post(setGoals);
-
 router.route('/:id').put(updateGoals).delete(deleteGoals)
 
 module.exports = router;
