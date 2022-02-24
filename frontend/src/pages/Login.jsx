129afad525fbde8204a1602da4f4d7f588afd1b4
Chris Carne <cjcarne@gmail.com>
2022-02-24 22:53:01 +0000

    goal form added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,13 @@
 import { useState, useEffect } from 'react'
 import { FaSignInAlt } from 'react-icons/fa'
 
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
+
+
 function Login() {
   const [formData, setFormData] = useState({
     email: '',
@@ -16,9 +23,36 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+    const userData = {
+      email,
+      password
+    }
+    dispatch(login(userData))
+
   }
 
   const { email, password } = formData
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+
+  }, [user, isSuccess, message, isError, navigate, dispatch]
+
+  )
+  if (isLoading) {
+    return Spinner
+  }
   return (
     <>
       <section className="heading">
